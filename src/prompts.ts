500702e2e5c35a3e5a6986a904b8c6e303ffe1b4
Dhanush.S <132473127+Dhxnushh@users.noreply.github.com>
2024-03-05 20:47:07 +0530

    lol

--- src/prompts.ts
+++ src/prompts.ts
@@ -1,18 +1,18 @@
 const readline = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+  input: process.stdin,
+  output: process.stdout,
+});
 
-
-export async function prompting(chain:any){
-    let qn = String;
-    readline.question("Ask question[Press Q to exit]: ", async (qn: string) => {
-    if((qn=="q") || (qn=="Q")){
+export async function prompting(chain: any) {
+  let qn = String;
+  readline.question("Ask question[Press Q to exit]: ", async (qn: string) => {
+    if (qn == "q" || qn == "Q") {
       console.log("Exiting chat.Thank you!"), readline.close();
-      return
+      return;
     }
     const res = (await chain).invoke({
       input: qn,
     });
     console.log((await res).answer), readline.close();
-})};
+  });
+}
